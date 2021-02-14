6714082917c984e8e7f7c1ffc6d39f6df519d3ce
Naoki0722 <mattu.nao722@gmail.com>
2021-02-14 22:58:18 +0900

    一部コード修正_2

--- src/views/PurchaseThanks.vue
+++ src/views/PurchaseThanks.vue
@@ -4,7 +4,7 @@
     <div class="absolute top-1/4 md:top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <h2 class="text-center w-screen px-4 md:px-0 text-base md:text-3xl font-serif text-yellow-900 mb-6 md:mb-0">購入いただきありがとうございました！</h2>
       <div class="md:my-11 text-center">
-        <button class="tracking-wider border hover:bg-orange rounded-full hover:text-white px-6 py-2 transition duration-300 ease-in-out bg-transparent text-gray-800 border-orange">
+        <button class="tracking-wider border hover:bg-orange rounded-full hover:text-white px-6 py-2 transition duration-300 ease-in-out bg-transparent text-gray-800 border-orange" @click="$router.push('/home')">
           Homeへ戻る
         </button>
       </div>
